6108bae313347e1fde6d6497ece6d88cedcbdab4
Lyall <lyall.vanderlinde@iriesoft.net>
2024-03-22 09:49:50 +0200

    prisma sqlite test

---
+++ packages/offline-sql-db/prisma/migrations/20240321121351_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" STRING NOT NULL,
+    "email" STRING NOT NULL,
+    "name" STRING,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" STRING NOT NULL,
+    "title" STRING NOT NULL,
+    "content" STRING NOT NULL,
+    "published" BOOL NOT NULL DEFAULT false,
+    "authorId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
